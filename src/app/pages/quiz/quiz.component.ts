a9f1615c6804f9689eaec15ff130ab75a884afba
javy10 <javiierb6@gmail.com>
2023-05-30 16:31:41 -0600

    PORTAL: MOSTRANDO LAS PREGUNTAS Y RESPUESTAS SELECCIONADAS

--- src/app/pages/quiz/quiz.component.ts
+++ src/app/pages/quiz/quiz.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { EvaluacionesService } from 'src/app/services/evaluaciones.service';
 
 @Component({
@@ -11,7 +11,7 @@ export class QuizComponent implements OnInit {
   listadoPreguntas: any[] = [];
   listadoRespuestas: any[] = [];
   colaborador_id:any = localStorage.getItem('id');
-
+  @ViewChild('container') container!: ElementRef;
   datos:any;
 
   constructor(private evaluacionService: EvaluacionesService, ) {
@@ -23,26 +23,62 @@ export class QuizComponent implements OnInit {
   }
 
   loadPreguntas() {
-    //console.log(this.colaborador_id)
-    const informacion: any[] = [];
+    
     this.evaluacionService.obtenerPreguntas(this.colaborador_id).subscribe((data: any) => {
-      
-      const result = data.dataDB.reduce((acc:any, item:any) => {
-        const existingItem = acc.find((i:any) => i.valorPregunta === item.valorPregunta);
-        if (existingItem) {
-          existingItem.valorRespuesta.push(item.valorRespuesta);
+      console.log(data.dataDB)
+
+      const preguntasUnicas = data.dataDB.reduce((acc:any, pregunta:any) => {
+        const preguntaExistente = acc.find((p:any) => p.valorPregunta === pregunta.valorPregunta);
+        if (preguntaExistente) {
+          preguntaExistente.valorRespuestas.push(pregunta.valorRespuesta);
+          preguntaExistente.respuesta_id.push(pregunta.respuesta_id);
         } else {
           acc.push({
-            valorPregunta: item.valorPregunta,
-            valorRespuesta: [item.valorRespuesta]
+            pregunta_id: pregunta.id,
+            valorPregunta: pregunta.valorPregunta,
+            valorRespuestas: [pregunta.valorRespuesta],
+            respuesta_id: [pregunta.respuesta_id],
+            tipoPregunta_id: pregunta.tipoPregunta_id,
+            selectedAnswers: []
           });
         }
         return acc;
       }, []);
-      this.listadoPreguntas = result;
-      console.log(result);
-      // console.log(this.listadoPreguntas);
+      
+      console.log(preguntasUnicas);
+      this.listadoPreguntas = preguntasUnicas;
+      console.log(this.listadoPreguntas);
 
     });
   }
+
+
+  
+  guardarRespuestas() {
+
+    const containerElement = this.container.nativeElement;
+    const cardElements = containerElement.querySelectorAll('.card');
+  
+    cardElements.forEach((cardElement: HTMLElement, index: number) => {
+      const preguntaId = this.listadoPreguntas[index].pregunta_id;
+      const tipoPregunta_id = this.listadoPreguntas[index].tipoPregunta_id;
+  
+      const radioInputs = cardElement.querySelectorAll('input[type="radio"]:checked');
+      const radioRespuestasSeleccionadas = Array.from(radioInputs).map((input: Element) => (input as HTMLInputElement).value);
+  
+      const checkboxInputs = cardElement.querySelectorAll('input[type="checkbox"]:checked');
+      const checkboxRespuestasSeleccionadas = Array.from(checkboxInputs).map((input: Element) => (input as HTMLInputElement).value);
+
+      this.datos = {
+        'pregunta': preguntaId,
+        'tipoPregunta_id': tipoPregunta_id,
+        'respuestaSeleccionadaR': radioRespuestasSeleccionadas,
+        'respuestaSeleccionadaC': checkboxRespuestasSeleccionadas
+      }
+      console.log(this.datos)
+    });
+
+  }
+
 }
+
